b25cb1a1af62bad481af3515fa63d75bd7427c69
Ömer Faruk KÖSE <65190873+farukkse@users.noreply.github.com>
2023-05-11 19:36:25 +0300

    hoca

--- assets/js/uno.js
+++ assets/js/uno.js
@@ -7,10 +7,10 @@ let table;
 // Create cards for four color
 for(let i =0; i<= 9; i++){
 
-    cards.push({color: 'green', number: i});
-    cards.push({color: 'yellow', number: i});
-    cards.push({color: 'red', number: i});
-    cards.push({color: 'blue', number: i});
+    cards.push({color: 'green', number: i.toString()});
+    cards.push({color: 'yellow', number: i.toString()});
+    cards.push({color: 'red', number: i.toString()});
+    cards.push({color: 'blue', number: i.toString() });
 
 }
 
@@ -29,7 +29,6 @@ for(let i = 1 ; i <= 7; i++){
 
 // table card
 table = cards.shift();
-
 
 deck = cards;
 
@@ -86,12 +85,87 @@ function isCardPlayable(cardColor,cardNumber) {
     return false
 }
 
+function changeTurn() {
+    if (turn === 1) {
+        turn = 2;
+        
+    }else{
+        turn = 1;
+    }
+}
+
+function CanCurrentUserPlay() {
+    let currentPlayerCard;
+
+    if (turn === 1) {
+        currentPlayerCard = document.querySelectorAll('.player .card')
+    } else {
+
+        currentPlayerCard = document.querySelectorAll('.cpu .player')
+        
+    }
+    for(let card of currentPlayerCard){
+        if(!isCardPlayable(card.dataset.color, card.dataset.number))
+
+        return true;
+    }
+    return false;
+}
+
+let isDeckUsed = false;
 // play Card
 function playCard() {
     
-    if(isCardPlayable(this.dataset.color, this.dataset.number)){
+    if (this.parentNode.classList.contains('table')) {
+        return;
+    }
+
+
+    if (this.parentNode.classList.contains('deck')) {
+        if (isDeckUsed === true) {
+            
+            return;
+        }
+
+
+        if(turn ===1){
+        //  handToAppend = playerContainer;
+        playerContainer.appendChild(this);
+
+        }else{
+            // handToAppend = cpuContainer
+            cpuContainer.appendChild(this)
+        }
+
+        isDeckUsed = true;
+
+
+        // if(!isCardPlayable(this.dataset.color, this.dataset.number) === table.color , table.number){
+        //     changeTurn();
+        // }
+
+        if (!CanCurrentUserPlay()) {
+            changeTurn();
+        }
+        return;
+    }
+
+
+    if(!isCardPlayable(this.dataset.color, this.dataset.number)){
+        return;
+    }
+    if (turn === 1 && this.parentNode.classList.contains('cpu')) {
+        return;
+    }
+    if (turn === 2 && this.parentNode.classList.contains('player')) {
+        return;
+    }
+
+    changeTurn();
+
+    
+    isDeckUsed = false;
         tableontainer.appendChild(this);
         table.color = this.dataset.color;
-        table.number = this.dataset.number
-    }
+        table.number = this.dataset.number;
 }
